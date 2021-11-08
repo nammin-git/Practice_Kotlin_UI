6a1d1ee2fb6946fb6ae9abdc3db715fc1064cc04
Administrator <nammk0121@gmail.com>
2021-11-08 23:30:41 +0900

    search page

--- app/src/main/java/com/example/login_display/SignupFragment.kt
+++ app/src/main/java/com/example/login_display/SignupFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SignupBlankFragment.newInstance] factory method to
+ * Use the [SignupFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SignupBlankFragment : Fragment() {
+class SignupFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class SignupBlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_blank, container, false)
+        return inflater.inflate(R.layout.fragment_signup, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class SignupBlankFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupBlankFragment.
+         * @return A new instance of fragment SignupFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SignupBlankFragment().apply {
+            SignupFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
